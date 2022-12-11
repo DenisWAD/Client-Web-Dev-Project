d6a24e075474d95eab54db707c7788cc8297fcec
DenisWAD <denismurray2@gmail.com>
2022-12-11 02:05:35 +0000

    Home Page and structure 'almost' done

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,3 +1,13 @@
 function openHamburgerNav() {
+    var dropdown_navbar = document.getElementById("navbar_dropdown")
     
+    if(dropdown_navbar.style.display==="none"){
+        dropdown_navbar.style.display="block";
+    }
+    else {
+        dropdown_navbar.style.display="none";
+    }
+    // console.log("Hi there");
+
+
 }
